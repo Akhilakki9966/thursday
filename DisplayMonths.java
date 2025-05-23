fa5ca820fa4fb35b1af1651e618e74477b304a26
Akhilakki9966 <Akhilakki9966@gmail.com>
2025-05-23 15:31:58 +0530

    Create DisplayMonths.java

---
+++ DisplayMonths.java
@@ -0,0 +1,13 @@
+public class DisplayMonths {
+    public static void main(String[] args) {
+        String[] months = {
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        };
+
+        System.out.println("Months of the Year:");
+        for (String month : months) {
+            System.out.println(month);
+        }
+    }
+}
