e74dbc96ffee9edb552ade5fc85a2f39040fd77d
Akhilakki9966 <Akhilakki9966@gmail.com>
2025-06-10 16:33:17 +0530

    Create WeekDaysDisplay.java

---
+++ WeekDaysDisplay.java
@@ -0,0 +1,17 @@
+import java.time.DayOfWeek;
+
+public class WeekDaysDisplay {
+
+    public static void main(String[] args) {
+        // Display weekdays (Monday to Friday)
+        System.out.println("The weekdays are:");
+        
+        // Loop through the days of the week starting from Monday
+        for (DayOfWeek day : DayOfWeek.values()) {
+            // We can skip Saturday and Sunday
+            if (day != DayOfWeek.SATURDAY && day != DayOfWeek.SUNDAY) {
+                System.out.println(day);
+            }
+        }
+    }
+}
